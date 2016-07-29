093047bc3fa45f108622a484271dc99282cdc9b9
liuwei <i@liuwei.co>
2016-07-29 11:34:31 +0800

    Initial Commit

---
+++ OpenSSL-lm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenSSL-lm
+//
+//  Created by 刘伟 on 16/7/29.
+//  Copyright © 2016年 Linkim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
